6371cda5f405674376a2e5fa6959286a8cb95bd5
rummanwaqar <rumman.waqar05@gmail.com>
2018-12-04 20:23:16 -0700

    made training file

--- train.py
+++ train.py
@@ -1,5 +1,19 @@
 from dataset_utils import get_samples, generator
 
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+
+def get_model():
+    model = Sequential()
+    model.add(Flatten(input_shape=(160,320,3)))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    print(model.summary())
+    return model
+
+
 def train():
     # read data
     datasets = ['data_1', 'data_2', 'data_3']
@@ -9,6 +23,11 @@ def train():
     training_generator = generator(training_samples, batch_size=32)
     validation_generator = generator(validation_samples, batch_size=32)
 
+    model = get_model()
+    model.fit_generator(training_generator, samples_per_epoch=len(training_samples),
+                        validation_data=validation_generator, nb_val_samples=len(validation_samples),
+                        nb_epoch=10)
+    model.save('model.h5')
 
 if __name__ == '__main__':
     train()
