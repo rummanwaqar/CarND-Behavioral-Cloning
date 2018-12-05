a2ae1a822f3a32cc7e3e63eebd6d745f40172e93
rummanwaqar <rumman.waqar05@gmail.com>
2018-12-04 21:18:35 -0700

    updated to nvidia model

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore")
+
 from dataset_utils import get_samples, generator
 
 from keras.models import Sequential
@@ -14,7 +17,17 @@ def get_model():
     # normalize from -1 to 1
     model.add(Lambda(lambda x: (x / 127.5) - 1.0, input_shape=(160,320,3)))
     model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+    # convolutional layers
+    model.add(Conv2D(24, kernel_size=5, strides=(2, 2), padding='valid', activation='relu'))
+    model.add(Conv2D(36, kernel_size=5, strides=(2, 2), padding='valid', activation='relu'))
+    model.add(Conv2D(48, kernel_size=5, strides=(2, 2), padding='valid', activation='relu'))
+    model.add(Conv2D(64, kernel_size=3, strides=(1, 1), padding='valid', activation='relu'))
+    model.add(Conv2D(64, kernel_size=3, strides=(1, 1), padding='valid', activation='relu'))
+    # fc layers
     model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam')
     print(model.summary())
