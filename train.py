63bfb4d65d3fe320a36ac20be9ef8e20bee1635e
rummanwaqar <rumman.waqar05@gmail.com>
2018-12-05 15:43:12 -0700

    elu

--- train.py
+++ train.py
@@ -13,29 +13,29 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import TensorBoard
 
-NAME = 'cnn-5x3-init-{}'.format(int(time.time()))
+NAME = 'cnn-5x3-elu-{}'.format(int(time.time()))
 
 def get_model():
     model = Sequential()
     # normalize from -1 to 1
     model.add(Lambda(lambda x: (x / 127.5) - 1.0, input_shape=(64,64,3)))
     # convolutional layers
-    model.add(Conv2D(24, kernel_size=5, strides=(2, 2), padding='valid', activation='relu', kernel_initializer='truncated_normal'))
+    model.add(Conv2D(24, kernel_size=5, strides=(2, 2), padding='valid', activation='elu', kernel_initializer='truncated_normal'))
     model.add(Dropout(0.5))
-    model.add(Conv2D(36, kernel_size=5, strides=(2, 2), padding='valid', activation='relu', kernel_initializer='truncated_normal'))
+    model.add(Conv2D(36, kernel_size=5, strides=(2, 2), padding='valid', activation='elu', kernel_initializer='truncated_normal'))
     model.add(Dropout(0.5))
-    model.add(Conv2D(48, kernel_size=5, strides=(2, 2), padding='valid', activation='relu', kernel_initializer='truncated_normal'))
+    model.add(Conv2D(48, kernel_size=5, strides=(2, 2), padding='valid', activation='elu', kernel_initializer='truncated_normal'))
     model.add(Dropout(0.5))
-    model.add(Conv2D(64, kernel_size=3, strides=(1, 1), padding='valid', activation='relu', kernel_initializer='truncated_normal'))
+    model.add(Conv2D(64, kernel_size=3, strides=(1, 1), padding='valid', activation='elu', kernel_initializer='truncated_normal'))
     model.add(Dropout(0.5))
-    model.add(Conv2D(64, kernel_size=3, strides=(1, 1), padding='valid', activation='relu', kernel_initializer='truncated_normal'))
+    model.add(Conv2D(64, kernel_size=3, strides=(1, 1), padding='valid', activation='elu', kernel_initializer='truncated_normal'))
     # fc layers
     model.add(Flatten())
-    model.add(Dense(200, activation='relu', kernel_initializer='truncated_normal'))
+    model.add(Dense(200, activation='elu', kernel_initializer='truncated_normal'))
     model.add(Dropout(0.5))
-    model.add(Dense(50, activation='relu', kernel_initializer='truncated_normal'))
+    model.add(Dense(50, activation='elu', kernel_initializer='truncated_normal'))
     model.add(Dropout(0.5))
-    model.add(Dense(10, activation='relu', kernel_initializer='truncated_normal'))
+    model.add(Dense(10, activation='elu', kernel_initializer='truncated_normal'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer=Adam(lr=1e-4))
@@ -52,7 +52,7 @@ def train():
     # read data
     datasets = get_dataset_names()
     BATCH_SIZE = 64
-    EPOCHS = 15
+    EPOCHS = 7
     training_samples, validation_samples = get_samples(datasets=datasets,
                                                        split=0.2,
                                                        base_url='data',
